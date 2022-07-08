d6c8c292f5343463e6cd281e1dfaf88b8e80a825
jonathan Ramos Pinel <jonyrampi@gmail.com>
2022-07-08 19:50:29 +0200

    working on a new easier version

--- main.js
+++ main.js
@@ -1,47 +1,53 @@
-const playerText = document.querySelector("#playerText");
-const computerText = document.querySelector("#computerText");
-const resultText = document.querySelector("#resultText");
-const choiceBtns = document.querySelectorAll(".choiceBtn");
-let player;
-let computer;
-let result;
 
-choiceBtns.forEach(button => button.addEventListener("click", () => {
+const options = document.querySelector(".options")
 
-    player = button.textContent;
-    computerTurn();
-    playerText.textContent = `Player: ${player}`;
-    computerText.textContent = `Computer: ${computer}`;
-    resultText.textContent = checkWinner();
-}));
-
-function computerTurn(){
-
-    const randNum = Math.floor(Math.random() * 3) + 1;
-
-    switch(randNum){
-      case 1:
-        computer = "rock";
-        break;
-      case 2:
-        computer = "paper";
-        break;
-      case 3:
-        computer = "scissors";
-        break;
-    }
+const gameOver = function() {
+    options.style.display = 'none';
 }
-function checkWinner(){
-    if(player == computer){
-      return "Tie";
+
+let moves = 0
+    
+    
+    
+    function computerPlay(){
+        const movesLeft = document.querySelector('.movesleft');
+        moves++;
+        movesLeft.innerText = `Moves Left: ${10-moves}`;  
+        
+        const randNum = Math.floor(Math.random() * 3) + 1;
+    
+        switch(randNum){
+          case 1:
+            computer = "Rock";
+            document.getElementById("computer-choice").src = "rock.png";
+            break
+          case 2:
+            computer = "Paper";
+            document.getElementById("computer-choice").src = "paper.png";
+            break
+          case 3:
+            computer = "Scissors";
+            document.getElementById("computer-choice").src = "scissors.png";
+            break;
+        }
+        
+    if(moves == 10){
+      gameOver();
     }
-    else if(computer == "rock"){
-      return (player == "paper") ? "You Win" : "You Lose!"
+  }
+
+
+    function chooseRock() {
+        document.getElementById("my-choice").src = "rock.png"
+        computerPlay()       
     }
-    else if(computer == "paper"){
-      return (player == "scissors") ? "You Win" : "You Lose!"
+    function choosePaper() {
+        document.getElementById("my-choice").src = "paper.png";
+        computerPlay()
     }
-    else if(computer == "scissors"){
-      return (player == "rock") ? "You Win" : "You Lose!"
+    function chooseScissors() {
+        document.getElementById("my-choice").src = "scissors.png";
+        computerPlay()
     }
-}
+    
+    
