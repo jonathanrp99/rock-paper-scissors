4de923b3ca7b6244b5bc65b1c2ef2716be30ad0a
jonathan Ramos Pinel <jonyrampi@gmail.com>
2022-07-08 15:23:07 +0200

    the game works

--- main.js
+++ main.js
@@ -1,22 +1,47 @@
-    const playerText = document.querySelector('#playerText');
-    const computerText = document.querySelector('#computerText');
-    const resultText = document.querySelector('#resultText');
-    const choiceBtns = document.querySelectorAll('.choices');
-    
+const playerText = document.querySelector("#playerText");
+const computerText = document.querySelector("#computerText");
+const resultText = document.querySelector("#resultText");
+const choiceBtns = document.querySelectorAll(".choiceBtn");
+let player;
+let computer;
+let result;
 
-function computerPlay() {
-    const randomNum = Math.floor(Math.random() * 3) + 1;
+choiceBtns.forEach(button => button.addEventListener("click", () => {
 
-    switch(randomNum) {
-        case 1:
-            computer = 'rock'
-        break
-        case 2:
-            computer = 'paper'
-        break
-        case 3:
-            computer = 'scissor'
-        break
+    player = button.textContent;
+    computerTurn();
+    playerText.textContent = `Player: ${player}`;
+    computerText.textContent = `Computer: ${computer}`;
+    resultText.textContent = checkWinner();
+}));
+
+function computerTurn(){
+
+    const randNum = Math.floor(Math.random() * 3) + 1;
+
+    switch(randNum){
+      case 1:
+        computer = "rock";
+        break;
+      case 2:
+        computer = "paper";
+        break;
+      case 3:
+        computer = "scissors";
+        break;
     }
-    
+}
+function checkWinner(){
+    if(player == computer){
+      return "Tie";
+    }
+    else if(computer == "rock"){
+      return (player == "paper") ? "You Win" : "You Lose!"
+    }
+    else if(computer == "paper"){
+      return (player == "scissors") ? "You Win" : "You Lose!"
+    }
+    else if(computer == "scissors"){
+      return (player == "rock") ? "You Win" : "You Lose!"
+    }
 }
