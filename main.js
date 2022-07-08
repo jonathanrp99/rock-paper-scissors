d29ac146782b9f53c993bc890c058f3a8b1e6f27
jonathan Ramos Pinel <jonyrampi@gmail.com>
2022-07-08 14:43:05 +0200

    WIP

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+    const playerText = document.querySelector('#playerText');
+    const computerText = document.querySelector('#computerText');
+    const resultText = document.querySelector('#resultText');
+    const choiceBtns = document.querySelectorAll('.choices');
+    
+
 function computerPlay() {
+    const randomNum = Math.floor(Math.random() * 3) + 1;
+
+    switch(randomNum) {
+        case 1:
+            computer = 'rock'
+        break
+        case 2:
+            computer = 'paper'
+        break
+        case 3:
+            computer = 'scissor'
+        break
+    }
     
 }
